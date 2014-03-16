d6bcf5119b42cfeba95119478cddadb60ec05c23
isaac <mystikum@gmail.com>
2014-03-16 08:39:56 -0400

    updated app

--- site/assets/js/app.js
+++ site/assets/js/app.js
@@ -11,64 +11,7 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
 DS.RESTAdapter.reopen({
   namespace: 'api'
 });
-/*
-App.ApplicationAdapter = DS.RESTAdapter.extend({
-	url : 'http://localhost:5000',
-	namespace : 'api',
-	serializer : DS.RESTSerializer.extend({
-		primaryKey : function(type) {
-			conosle.log('prim');
-			return '_id';
-		},
-	   extract: function(loader, json, type, record) {
-	   	console.log('ass')
-      var newJSON, root;
-      root = this.rootForType(type);
-      newJSON = {};
-      newJSON[root] = json;
-      json = newJSON;
-      this.sideload(loader, type, json, root);
-      this.extractMeta(loader, type, json);
-      if (json[root]) {
-        if (record) {
-          loader.updateId(record, json[root]);
-        }
-        return this.extractRecordRepresentation(loader, type, json[root]);
-      } else {
-        return Ember.Logger.warn("Extract requested, but no data given for " + type + ". This may cause weird problems.");
-      }
-    },
-    extractMany: function(loader, json, type, records) {
-      var i, newJSON, objects, reference, references, root;
-      root = this.rootForType(type);
-      root = this.pluralize(root);
-      newJSON = {};
-      newJSON[root] = json;
-      json = newJSON;
-      this.sideload(loader, type, json, root);
-      this.extractMeta(loader, type, json);
-      if (json[root]) {
-        objects = json[root];
-        references = [];
-        if (records) {
-          records = records.toArray();
-        }
-        i = 0;
-        while (i < objects.length) {
-          if (records) {
-            loader.updateId(records[i], objects[i]);
-          }
-          reference = this.extractRecordRepresentation(loader, type, objects[i]);
-          references.push(reference);
-          i++;
-        }
-        return loader.populateArray(references);
-      }
-    }
-   
-	})
-});
-*/
+
 App.Store = DS.Store.extend({
 	revision : 13
 	//adapter : App.ApplicationAdapter.create()
